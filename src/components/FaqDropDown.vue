cc9a78e69099773b06daf82a82b45f28d5333331
Danny-607 <danielhudson1294@gmail.com>
2024-05-03 11:37:25 +0100

    FAQ page styling, faq component, faq dropdown functionality

---
+++ src/components/FaqDropDown.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- faq drop down menu -->
+  <div class="faq-item">
+    <button class="faq-btn" @click="toggleVisibility">{{ question }}</button>
+    <div v-if="isVisible" class="faq-answer">
+      <p>{{ answer }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FaqDropDown',
+  props: {
+    question: {
+      type: String,
+      required: true
+    },
+    answer: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isVisible: false
+    }
+  },
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible
+    }
+  },
+  mounted() {}
+}
+</script>
+
+<style scoped>
+.faq-item {
+  margin-bottom: 1rem;
+  width: 100%;
+}
+.faq-btn {
+  padding: 1rem;
+  background-color: #e0e0e0;
+  width: 100%;
+  text-align: left;
+  border: none;
+  cursor: pointer;
+}
+.faq-answer {
+  padding: 1rem;
+  background-color: #f0f0f0;
+  width: 100%;
+  color: black;
+}
+</style>
--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -1,16 +1,54 @@
 <template>
-  <div>
-    <!-- Your HTML template code goes here -->
+  <hero-component header="FAQ" />
+  <section class="information">
+    <h1>Answering your questions!</h1>
+    <p>
+      Want to know more about the services we offer? Maybe you have specific questions about our
+      processes that you’d like to learn more about? Or perhaps you just need clarification on a few
+      details. Whatever the case, the team at Dubai-Landscapes is on hand to help answer any
+      questions you have. Check out our list of frequently asked questions below; if your question
+      is not covered feel free to contact our team directly. Drop us a message on WhatsApp or give
+      us a call.
+    </p>
+  </section>
+  <div class="back-drop">
+    <FaqDropDown question="What is the meaning of life?" answer="42" />
+    <FaqDropDown
+      question="What is the airspeed velocity of an unladen swallow?"
+      answer="African or European?"
+    />
   </div>
 </template>
 
 <script>
+import FaqDropDown from '@/components/FaqDropDown.vue'
 export default {
-  name: 'FaqView'
-  // Your component's properties, methods, etc. go here
+  name: 'FaqView',
+  components: {
+    FaqDropDown
+  }
 }
 </script>
 
 <style scoped>
-/* Your component's CSS styles go here */
+.information {
+  padding: 1rem;
+  grid-column: 1 / span 2;
+  background-color: #4b4632;
+  color: white;
+  font-size: 1.5rem;
+}
+.back-drop {
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 1rem;
+  width: 80%;
+  border-radius: 5px;
+  color: white;
+  grid-column: span 2;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  justify-self: center;
+}
 </style>
