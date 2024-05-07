6b1e7740a9a7204e9949cae8069caf019db1fde6
Danny-607 <danielhudson1294@gmail.com>
2024-05-07 06:34:51 +0100

    Changed images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,20 +3,20 @@
   <ContentLayoutRight
     header="UAE's Largest Landscapers"
     paragraph="Your garden or outdoor space is an extension of your entire home. A place where you can relax after a long day, entertain friends and family or socialise with loved ones. At Dubai-Landscapes,we’ve spent a decade in the UAE, crafting bespoke outdoor living spaces that achieve exactly what our clients desire"
-    image="/images/hero.jpg"
+    image="/images/016A3170.jpg"
   />
   <ContentLayoutLeft
     header="Hard and soft landscaping services"
     paragraph="Not only can we offer the design and installation of a number of soft and hard landscaping elements, but we’ll craft an outdoor living space 
 that looks amazing, while is also functional and practical for your family to use. From outdoor design elements like paving, decking and Easigrass 
 artificial turf to luxurious seating and dedicated barbecue areas, we can do it all."
-    image="/images/hero.jpg"
+    image="/images/016A3216-HDR.jpg"
   />
   <ContentLayoutRight
     header="UAE's Largest Landscapers"
     paragraph="So wherever you’re based in the UAE, contact Dubai-Landscapes today to find 
 out more about our range of landscaping services and bespoke gardens. Message us on WhatsApp and a member of our team will be in touch"
-    image="/images/hero.jpg"
+    image="/images/016A3298.jpg"
   />
   <ContentLayoutLeft
     header="British Design and Installation Services"
@@ -25,7 +25,7 @@ Dubai and the surrounding areas. Providing our customers with leading British de
 of outdoor living elements, we always create customised spaces and bespoke gardens that perfectly match the unique specifications
  of our clients’ outdoor spaces, and align with your vision. Over the years, we’ve earned a reputation for being the premier landscaping 
 company in the UAE. "
-    image="/images/hero.jpg"
+    image="/images/016A3289.jpg"
   />
 </template>
 
